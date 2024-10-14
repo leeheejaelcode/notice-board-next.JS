36ec770ecc7444ff9229fc453f496b753b18af34
heejae <leeheejaelcode@gmail.com>
2024-10-14 17:43:14 +0900

    [기능] params를 가지고 dynamic route 구현

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,26 +1,15 @@
 import { connectDB } from "@/util/database";
-import Link from "next/link";
+import { ObjectId } from "mongodb";
 
-export default async function List() {
+export default async function Detail({ params }: { params: { id: string } }) {
+  const { id } = params;
   const db = (await connectDB).db("forum");
-  const result = await db.collection("post").find().toArray();
-
+  const result = await db.collection("post").findOne({ _id: new ObjectId(id) }); // id 사용
   return (
-    <div className="bg-[rgb(249,250,255)] p-[10px] h-screen">
-      {result.map((item) => {
-        return (
-          <div
-            className="bg-white rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(,224,224)]"
-            key={item._id.toString()}>
-            <Link
-              href={`/detail/${item._id}`}
-              className="font-bold text-[20px]">
-              {item.title}
-            </Link>
-            <p className="text-gray-400 my-[5px]">1월 1일</p>
-          </div>
-        );
-      })}
+    <div>
+      <h4 className="font-bold text-[50px]">상세페이지</h4>
+      <h4 className="font-bold text-[20px]">제목 : {result?.title}</h4>
+      <h4 className="text-[20px]">내용 : {result?.content}</h4>
     </div>
   );
 }
