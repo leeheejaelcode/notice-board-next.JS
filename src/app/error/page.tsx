86871aa4ba47a6f0cc13256a8223eeb973183076
heejae <leeheejaelcode@gmail.com>
2024-10-15 12:34:36 +0900

    [생성] error 페이지

---
+++ src/app/error/page.tsx
@@ -0,0 +1,7 @@
+export default function error() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <h1 className="font-bold text-red-500 text-[40px]">에러페이지 입니다</h1>
+    </div>
+  );
+}
