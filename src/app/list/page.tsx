e43552b6be7995ba61e2c502d22e147984f255d9
heejae <leeheejaelcode@gmail.com>
2024-10-15 18:36:33 +0900

    [수정] ListItem client component 분리
    - ListItem 컴포넌트 분리

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,28 +1,12 @@
 import { connectDB } from "@/util/database";
-import Link from "next/link";
-import DetailLink from "./DetailLink";
+import ListItem from "./ListItem";
 
 export default async function List() {
   const db = (await connectDB).db("forum");
   const result = await db.collection("post").find().toArray();
-
   return (
     <div className="bg-[rgb(249,250,255)] p-[10px] h-screen">
-      {result.map((item) => {
-        return (
-          <div
-            className="bg-white rounded-[10px] p-[20px] mb-[5px] shadow-[0_2px_4px_0px_rgb(,224,224)]"
-            key={item._id.toString()}>
-            <Link
-              href={`/detail/${item._id}`}
-              className="font-bold text-[20px]">
-              <h4>{item.title}</h4>
-            </Link>
-            <DetailLink pageId={item._id.toString()} />
-            <p className="text-gray-400 my-[5px]">1월 1일</p>
-          </div>
-        );
-      })}
+      <ListItem result={result} />
     </div>
   );
 }
