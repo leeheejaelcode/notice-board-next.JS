d7d956dc4140dd029966a3c01aa5252531c719b1
heejae <leeheejaelcode@gmail.com>
2024-10-15 12:36:09 +0900

    [기능] write페이지 공백일때는 실행이 되지 않는 기능

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,23 +1,40 @@
+"use client";
+
 export default function Write() {
+  const submitBtn = (e: React.FormEvent<HTMLFormElement>) => {
+    const title = document.getElementById("title") as HTMLInputElement;
+    const content = document.getElementById("content") as HTMLInputElement;
+    if (title.value === "" || content.value === "") {
+      alert("제목와 내용을 입력하십시오.");
+      e.preventDefault();
+    }
+  };
+
   return (
-    <div>
-      <h1>글작성</h1>
-      <form action="/api/test" method="post">
+    <div className="p-10">
+      <h1 className="mb-5 font-bold">글작성</h1>
+      <form action="/api/post/new" method="post" onSubmit={submitBtn}>
         <label htmlFor="title">제목</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          className="border border-solid border-black"
-        />
+        <div>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="border border-solid border-black p-[20px]"
+          />
+        </div>
         <label htmlFor="content">내용</label>
-        <input
-          type="text"
-          name="content"
-          id="content"
-          className="border border-solid border-black"
-        />
-        <button type="submit">글 발행</button>
+        <div>
+          <input
+            type="text"
+            name="content"
+            id="content"
+            className="border border-solid border-black p-[20px]"
+          />
+        </div>
+        <button type="submit" className="rounded-md bg-slate-300 p-2">
+          글 발행
+        </button>
       </form>
     </div>
   );
