376c94acc526217cbebe9b62f2467e2fe55ae9a3
heejae <leeheejaelcode@gmail.com>
2024-10-17 10:30:04 +0900

    [기능] mongoDBAdapter를 통한 로그인 기능

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,12 @@
+// [...nextauth].ts
+import { connectDB } from "@/util/database";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 const clientId = process.env.GITHUB_ID;
 const clientSecret = process.env.GITHUB_SECRET;
 const nextAuthSecret = process.env.NEXTAUTH_SECRET;
-
 if (!clientId || !clientSecret || !nextAuthSecret) {
   throw new Error("Missing environment variables for NextAuth");
 }
@@ -18,7 +20,11 @@ export const authOptions = {
     }),
   ],
   secret: nextAuthSecret,
+  adapter: MongoDBAdapter(connectDB),
 };
 export default NextAuth(authOptions);
 
 // 지금쓰는 라이브러리(NEXTAUTH)에서 별도에 셋팅이 없으면 기본이 JWT 방식입니다.
+// DB adapter 사용을 하게되면 session 방식 도입가능합니다.
+// 1. 첫로그인시 자동회원가입(DB보관)
+// 2. 로그인시 DB에 세션정보 보관
