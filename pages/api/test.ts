a76849273ed5d4c7f5595ee9485e94fd49eecc65
heejae <leeheejaelcode@gmail.com>
2024-10-17 10:32:00 +0900

    [학습] 쿼리스트링으로 서버에서 데이터 받는 방법

--- pages/api/test.ts
+++ pages/api/test.ts
@@ -16,8 +16,18 @@ export default async function handler(
     const result = await db.collection("post").find().toArray();
     return res.status(200).json(result);
   }
+
+  console.log(req.query);
 }
 
 // 서버기능 처리 성공시엔 status(200)
 // 서버기능 처리 실패시엔 status(500)
 // 서버기능 처리 실패시(유저잘못) status(400)
+
+// query string 장점
+// 간단함, GET요청으로도 데이터 전송이 가능합니다.
+// query string으로 받은 자료들은 req.query로 받을 수 있습니다.
+
+// query string 단점
+// 데이터가 많으면 더럽습니다.
+// URL에 데이터가 노출이 되기때문에 보안에 위험이 있습니다.
