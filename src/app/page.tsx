4c16d6c5fd4669d8530f0efc21d9796dcea6ff47
heejae <leeheejaelcode@gmail.com>
2024-10-17 10:32:45 +0900

    [주석] 안쓰는 코드 주석

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ export const revalidate = 60;
 
 export default async function Home() {
   // GET요청 결과 캐싱하기
-  await fetch("/URL", { cache: "force-cache" });
+  // await fetch("/URL", { cache: "force-cache" });
   // 사실은 await fetch("/url") 뒤에 cache 생략해도 캐싱이 가능합니다.
   // 실시간 데이터를 가져오게 하려면 {cache : "no-store"}
   // await fetch("/URL", {next : {revalidate : 60}}); 이러면 60초마다 캐싱된 데이터를 갱신해줍니다.
 
-  const db = (await connectDB).db("forum");
-  const result = await db.collection("post").find().toArray();
+  // const db = (await connectDB).db("forum");
+  // const result = await db.collection("post").find().toArray();
 
   /* -------------------------------------------------------------------------- */
   /*                             DB출력 결과 캐싱하는 방법 2가지                  */
