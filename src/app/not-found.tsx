df0836a76a55766a31ff4bbee8d257773ccb42b4
heejae <leeheejaelcode@gmail.com>
2024-10-18 15:03:39 +0900

    [학습] not-found.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-black">
+      <h1 className="font-bold text-green-500 text-[40px]">
+        없는 페이지입니다.
+      </h1>
+    </div>
+  );
+}
