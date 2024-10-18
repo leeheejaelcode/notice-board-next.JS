971653b3c4ceccc90b49fb3b9d0f560eeea6d0d5
heejae <leeheejaelcode@gmail.com>
2024-10-18 12:32:45 +0900

    [기능] 댓글목록 가져오는 서버기능 추가

--- pages/api/post/comment.ts
+++ pages/api/post/comment.ts
@@ -17,19 +17,50 @@ export default async function handler(
       return res.status(401).json({ message: "Not authenticated" });
     }
 
+    if (req.body.parent === "") {
+      return res.status(400).json({ message: "Parent ID is required" });
+    }
+
+    if (req.body.email === "") {
+      return res.status(400).json({ message: "email is required" });
+    }
+
     try {
       const data = {
+        comment: req.body.comment,
         parent: new ObjectId(req.body.parent),
         email: session?.user?.email,
-        comment: req.body.comment,
+        name: session?.user?.name,
       };
       await db.collection("comment").insertOne(data);
-      return res.status(200).json({ message: "Comment inserted successfully" });
+      const result = await db
+        .collection("comment")
+        .find({ parent: new ObjectId(req.body.parent) })
+        .toArray();
+      return res.status(200).json(result); // 데이터 반환
     } catch (error) {
       console.error("Error inserting comment:", error);
       return res.status(500).json({ message: "Error inserting comment" });
     }
-  } else {
-    return res.status(405).json({ message: "Method Not Allowed" });
+  }
+
+  if (req.method === "GET") {
+    const db = (await connectDB).db("forum");
+    const parentId = req.query.id; // 요청에서 부모 ID를 가져옵니다.
+
+    if (!parentId || typeof parentId !== "string") {
+      return res.status(400).json({ message: "Invalid Parent ID" });
+    }
+
+    try {
+      const result = await db
+        .collection("comment")
+        .find({ parent: new ObjectId(parentId) })
+        .toArray();
+      return res.status(200).json(result); // 데이터 반환
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+      return res.status(500).json({ message: "Error fetching comments" });
+    }
   }
 }
