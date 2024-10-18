c65705b37586406eafb3313c84346958ff7685cd
heejae <leeheejaelcode@gmail.com>
2024-10-18 12:35:24 +0900

    [학습] loading.tsx
    - 페이지 로드시 보여줄 loading.tsx 페이지

---
+++ src/app/detail/[id]/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div>
+      로딩중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    </div>
+  );
+}
+
+// loading.tsx
+// next.js에서 loading.tsx는 react의 Suspense 컴포넌트와 동일합니다
+// <Suspense fallback={<div>Loading...</div>}>(page.tsx내용)</Suspense>
